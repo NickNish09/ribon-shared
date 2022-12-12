dbf82ab888a10769247b7567a966cc06d3bb479a
Nicholas Marques <nnmarques97@gmail.com>
2022-12-12 14:25:40 -0300

    build: add prettier

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,17 @@
-import theme from "./theme";
-import normalize from "./normalize";
-import GlobalStyle from "./globalStyle";
-import { typography } from "./typography/typography";
+import theme from './theme';
+import normalize from './normalize';
+import GlobalStyle from './globalStyle';
+import { typography } from './typography/typography';
+import {
+  ThemeProviderComponent,
+  ThemeProviderNativeComponent,
+} from './themeProviderComponent';
 
-export { theme, normalize, GlobalStyle, typography };
+export {
+  theme,
+  normalize,
+  GlobalStyle,
+  typography,
+  ThemeProviderComponent,
+  ThemeProviderNativeComponent,
+};
