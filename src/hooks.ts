4370f8cdb569361fded443a1925da5df7bcbb05a
Nicholas Marques <nnmarques97@gmail.com>
2022-12-12 15:35:40 -0300

    feat: add index file

---
+++ src/hooks.ts
@@ -0,0 +1,37 @@
+import { useApi } from './hooks/useApi';
+import QueryClientComponent from './hooks/useApi/queryClient';
+import useCanDonate from './hooks/apiHooks/useCanDonate';
+import useCardGivingFees from './hooks/apiHooks/useCardGivingFees';
+import useCauses from './hooks/apiHooks/useCauses';
+import useContractBalance from './hooks/apiHooks/useContractBalance';
+import useCryptoTransaction from './hooks/apiHooks/useCryptoTransaction';
+import useDonations from './hooks/apiHooks/useDonations';
+import useImpact from './hooks/apiHooks/useImpact';
+import useIntegration from './hooks/apiHooks/useIntegration';
+import useNonProfitImpact from './hooks/apiHooks/useNonProfitImpact';
+import useNonProfits from './hooks/apiHooks/useNonProfits';
+import useOffers from './hooks/apiHooks/useOffers';
+import usePromoterCardGivings from './hooks/apiHooks/usePromoterCardGivings';
+import useSources from './hooks/apiHooks/useSources';
+import useStories from './hooks/apiHooks/useStories';
+import useUsers from './hooks/apiHooks/useUsers';
+
+export {
+  useApi,
+  QueryClientComponent,
+  useCanDonate,
+  useCardGivingFees,
+  useCauses,
+  useContractBalance,
+  useCryptoTransaction,
+  useDonations,
+  useImpact,
+  useIntegration,
+  useNonProfitImpact,
+  useNonProfits,
+  useOffers,
+  usePromoterCardGivings,
+  useSources,
+  useStories,
+  useUsers,
+};
