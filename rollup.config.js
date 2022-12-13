af5c05dad29ea78823a68b2c6a72df0653b7dd88
Nicholas Marques <nnmarques97@gmail.com>
2022-12-13 07:19:39 -0300

    feat: update src/index

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import sass from 'rollup-plugin-sass'
-import { uglify } from 'rollup-plugin-uglify'
-import typescript from 'rollup-plugin-typescript2'
+import sass from 'rollup-plugin-sass';
+import { uglify } from 'rollup-plugin-uglify';
+import typescript from 'rollup-plugin-typescript2';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
@@ -16,5 +16,18 @@ export default {
     },
   ],
   plugins: [sass({ insert: true }), typescript(), uglify()],
-  external: ['react', 'react-dom'],
-}
+  external: [
+    'react',
+    'react-dom',
+    'react-query',
+    'react-router-dom',
+    'styled-components',
+    'styled-components/native',
+    'axios',
+    'camelcase-keys',
+    'snakecase-keys',
+    '@apollo/client',
+    'cross-fetch',
+    'ethers',
+  ],
+};
--- src/services/apiTheGraph/index.tsx
+++ src/services/apiTheGraph/index.tsx
@@ -1,5 +1,5 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-import { THE_GRAPH_API } from "utils/constants";
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { THE_GRAPH_API } from 'utils/constants';
 import fetch from 'cross-fetch';
 
 const APIURL = THE_GRAPH_API;
@@ -7,5 +7,5 @@ const APIURL = THE_GRAPH_API;
 export const client = new ApolloClient({
   uri: APIURL,
   cache: new InMemoryCache(),
-  link: new HttpLink({uri: APIURL, fetch})
+  link: new HttpLink({ uri: APIURL, fetch }),
 });
