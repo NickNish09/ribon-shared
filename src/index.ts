d758371153d144ff3917c20c237eb2368fb1d2f5
Nicholas Marques <nnmarques97@gmail.com>
2022-12-13 07:57:57 -0300

    fix: add base64 atob

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import { decode, encode } from 'base-64';
 export * from './hooks';
 export * from './services';
+
+if (!global.btoa) {
+  global.btoa = encode;
+}
+
+if (!global.atob) {
+  global.atob = decode;
+}
